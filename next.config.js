19d354c73e0a4b732a6e9e1e7d87ef8ae9b1fde9
jayesh-bakle <jayeshbakleabd@gmail.com>
2023-12-06 21:42:26 +0530

    Add loaders for a pdf file

--- next.config.js
+++ next.config.js
@@ -2,3 +2,26 @@
 const nextConfig = {}
 
 module.exports = nextConfig
+// next.config.js
+module.exports = {
+    webpack: (config, { isServer }) => {
+      // Only use file-loader for client-side bundle
+      if (!isServer) {
+        config.module.rules.push({
+          test: /\.(pdf)$/,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                publicPath: '/_next',
+                name: 'static/media/[name].[hash].[ext]',
+              },
+            },
+          ],
+        });
+      }
+  
+      return config;
+    },
+  };
+  
